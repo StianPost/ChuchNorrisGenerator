6e1d6b235fa0525e37e72d0fd4ce0413d0721d60
Stian <69841774+StianPost@users.noreply.github.com>
2021-02-14 23:52:41 +0100

    added error functions

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 const randomJokes = 'http://api.icndb.com/jokes/random';
 const jokeBtn = document.querySelector('#generateJoke');
+
 jokeBtn.onclick = function () {
   async function getJokes(url) {
     try {
       response = await fetch(url);
       result = await response.json();
       const jokeRND = result.value.joke;
-
-      let jokeDivClear = '';
       document.querySelector('.jokes').innerHTML = `
       <p>${jokeRND}</p>
       `;
     } catch (error) {
       console.log(error);
+      document.querySelector('.alert').innerHTML = showAlertTouser(
+        `Couldn't find joke`,
+        'danger'
+      );
     } finally {
+      setTimeout(function () {
+        document.querySelector('.alert').innerHTML = '';
+      }, 3000);
     }
   }
   getJokes(randomJokes);
