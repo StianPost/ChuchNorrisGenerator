89be3be93a5ef80659e6e2df569f63a09005c75f
Stian <69841774+StianPost@users.noreply.github.com>
2021-02-14 23:49:04 +0100

    I fix

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 const randomJokes = 'http://api.icndb.com/jokes/random';
 const jokeBtn = document.querySelector('#generateJoke');
+jokeBtn.onclick = function () {
+  async function getJokes(url) {
+    try {
+      response = await fetch(url);
+      result = await response.json();
+      const jokeRND = result.value.joke;
 
-async function getJokes(url) {
-  try {
-    response = await fetch(url);
-    result = await response.json();
-    const jokeRND = result.value.joke;
-    jokeBtn.onclick = function () {
       let jokeDivClear = '';
       document.querySelector('.jokes').innerHTML = `
       <p>${jokeRND}</p>
       `;
-    };
-  } catch (error) {
-    console.log(error);
-  } finally {
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
   }
-}
-
-getJokes(randomJokes);
+  getJokes(randomJokes);
+};
